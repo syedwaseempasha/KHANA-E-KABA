3865ba57e9e13747fb7bf4ce068ad9602b0d13ae
syedwaseempasha <iam.syedwaseempasha@gmail.com>
2024-02-01 20:29:33 +0530

    user routes defined

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import connectDB from "./src/databse/db.js";
+import userRouter from "./src/routes/userRoute.js";
+
 
 
 const app = express();
@@ -15,9 +17,11 @@ app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+app.use("/users", userRouter); // http://localhost:5000/users/signup
+
 connectDB()
 .then(() =>{
-    const port = process.env.PORT || 5000
+    const port = process.env.PORT || 8001
     app.listen(process.env.port, () =>{
         console.log(`server is running on port : ${port} `);
     })
--- src/controllers/userCtlr.js
+++ src/controllers/userCtlr.js
@@ -1,9 +1,34 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import userModel from "../models/userModel.js";
 
-import UserModal from "../models/user.js";
+userModel
 
 const secret = "test";
+
+export const signin = async (req, res) => {
+    const { email, password } = req.body;
+  
+    try {
+      const oldUser = await UserModal.findOne({ email });
+      if (!oldUser)
+        return res.status(404).json({ message: "User doesn't exist" });
+  
+      const isPasswordCorrect = await bcrypt.compare(password, oldUser.password);
+  
+      if (!isPasswordCorrect)
+        return res.status(400).json({ message: "Invalid credentials" });
+  
+      const token = jwt.sign({ email: oldUser.email, id: oldUser._id }, secret, {
+        expiresIn: "1h",
+      });
+  
+      res.status(200).json({ result: oldUser, token });
+    } catch (error) {
+      res.status(500).json({ message: "Something went wrong" });
+      console.log(error);
+    }
+  };
 
 export const signup = async (req, res) => {
     const { email, password, firstName, lastName } = req.body;
